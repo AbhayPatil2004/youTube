31a6d00204d7ca4827ad4f491083391a8dffe5b8
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-11-21 20:39:49 +0530

    Database connection

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+// import express from 'express'
+// const app = express()
+
+// ( async () => {
+//     try{
+//         await mongoose.connect( `${process.env.MONGO_URL}/${DB_NAME}`)
+//         app.on("errr" , ( error ) => {
+//             console.log("Error" , error)
+//             throw error
+//         })
+
+//         app.listen( process.env.PORT , () => {
+//             console.log('App is Listening on port' , process.env.PORT )
+
+//         })
+//     }
+//     catch(error){
+//         console.error("Error" , error)
+//         throw error 
+//     }
+// })()
